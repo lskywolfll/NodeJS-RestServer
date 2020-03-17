46374ec84a37233f0269477db09a4f0e03dc6a1d
lskywolfll <renealejandrosanchezmorales@gmail.com>
2020-03-17 14:51:06 -0300

    se termina la subida de multiples archivos

--- server/routes/uploads.js
+++ server/routes/uploads.js
@@ -3,40 +3,89 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const extensionesValidas = ['jpg', 'png', 'pdf', 'jpeg'];
 // opciones por defecto y habilitacion de creacion de arcihvos 
-app.use( fileUpload({ useTempFiles: true }) );
+// Lo convierte todos los archivos qe se suban a req.files (LLegaran siempre a esa propiedad)
+app.use(fileUpload({ useTempFiles: true }));
 
 app.put('/upload', (req, res) => {
 
-    if(!req.files){
+    if (!req.files) {
         return res.status(400).json({
             ok: false,
             err: {
-                message: 'No seha seleccionado ningun archivo'
+                message: 'No se ha seleccionado ningun archivo!'
             }
         })
     }
 
-    // el nombre del input del cual toma el archivo en la pagina web es el nombre en la cual llegada
-    let archivoEjemplo = req.files.archivo;
 
-    archivoEjemplo.mv(`uploads/${archivoEjemplo.name}`, (err) => {
-        if(err){
-            return res.status(500).json({
-                ok: false,
-                err
-            });
-        }
+    if (req.files.archivos.length > 1) {
+        // Itero multiples archivos
+        req.files.archivos.forEach(archivo => {
+            let extension = archivo.mimetype.split('/')[1];
+
+            if (extensionesValidas.indexOf(extension) < 0) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Las extensiones permitidas son: ' + extensionesValidas.join(',')
+                    }
+                });
+            }
+
+            archivo.mv(`uploads/${archivo.name}`, (err) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+            })
+        });
 
         return res.status(200).json({
             ok: true,
             message: 'Se ha subido correctamente',
-            detalles: {
-                name: archivoEjemplo.name,
-                extension: archivoEjemplo.mimetype
-            }
+            archivos: req.files.archivos
         });
-    });
+    } else {
+        let archivoEjemplo = req.files.archivos;
+
+        let extension = archivoEjemplo.mimetype.split('/')[1];
+
+        if (extensionesValidas.indexOf(extension) < 0) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Las extensiones permitidas son: ' + extensionesValidas.join(',')
+                }
+            });
+        } else {
+            archivoEjemplo.mv(`uploads/${archivoEjemplo.name}`, (err) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                return res.status(200).json({
+                    ok: true,
+                    message: 'Se ha subido correctamente',
+                    detalles: {
+                        name: archivoEjemplo.name,
+                        extension: archivoEjemplo.mimetype
+                    }
+                });
+            });
+        }
+    }
+
+    // // el nombre del input del cual toma el archivo en la pagina web es el nombre en la cual llegada
+    // let archivoEjemplo = req.files.archivo;
+
+
 });
 
 module.exports = app
